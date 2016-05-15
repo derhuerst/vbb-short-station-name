6768ba0ea8dfb72e38c5253b6dc74b3f4d50d1e4
Jannis R <mail@jannisr.de>
2016-05-15 20:50:22 +0200

    implementation :clap:

--- index.js
+++ index.js
@@ -1,3 +1,20 @@
 'use strict'
 
-// todo
+const strasse = /(stra(ss|ß)e)[^\w]/i
+const bhf     = /[^\w]bhf(?=[^\w])/i
+const berlin  = /[^\w]\(berlin\)/i
+
+const shorten = (n) => {
+	n = ` ${n} ` // regexes work now
+	.replace(bhf, '')
+	.replace(berlin, '')
+
+	// …strasse -> …str.
+	let m = strasse.exec(n)
+	if (m) n =
+		  n.substr(0, m.index) + m[1].substr(0, 3) + '.'
+		+ n.substr(m.index + m[1].length)
+	return n.trim()
+}
+
+module.exports = shorten
